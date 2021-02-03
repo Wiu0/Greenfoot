fba516072278b14d76c11bf5624fe19324e1d645
lenovo e431 <lenovo e431>
2021-02-03 08:28:29 -0300

    Show 2 ways about how animate the World backgroud.

---
+++ animated-scenario/MyWorldMoveWithActor.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorldMoveWithActor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorldMoveWithActor extends World
+{
+
+    private int imageCount = 0;
+    private GreenfootImage bgImage = new GreenfootImage("game-seamless-background-flat-style-2d-vector-10644477.jpg");
+
+    /**
+     * Constructor for objects of class MyWorldMoveWithActor.
+     * 
+     */
+    public MyWorldMoveWithActor()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        //2771 X 980
+        //1000 x 780
+        super(1300, 650, 1); 
+        addObject(new Frog(), 50, 500);
+    }
+
+    public void move() {
+        imageCount -= 1; //(or any other value; small -> slow moving, big -> fast movement)
+        drawBackgroundImage();
+    }
+
+    public void drawBackgroundImage() {
+       if (imageCount < -bgImage.getWidth()) {
+            imageCount += bgImage.getWidth();
+        }
+        int temp = imageCount;
+        getBackground().drawImage(bgImage, temp, 0);
+        getBackground().drawImage(bgImage, temp + bgImage.getWidth(), 0);
+    }
+
+}
