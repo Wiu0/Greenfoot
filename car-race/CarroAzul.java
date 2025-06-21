6c976b78022a96d7826f31319ddbd39ccc0f44e9
Henrique Coelho Gama <20881939+Wiu0@users.noreply.github.com>
2025-06-20 21:56:55 -0300

    add new car game

---
+++ car-race/CarroAzul.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CarroAzul here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CarroAzul extends Actor
+{
+    /**
+     * Act - do whatever the CarroAzul wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX(), getY() + 6);     
+        if(isTouching(Car.class)) {
+            Greenfoot.stop();
+            getWorld().showText("GAME OVER", getWorld().getWidth()/2, getWorld().getHeight()/2);
+            MyWorld.gs.stop();
+            new GreenfootSound("hit.mp3").play();
+        }
+        if(isAtEdge()) {
+            getWorld().removeObject(this);
+            MyWorld.PONTOS++;
+        }
+    }
+}
---
+++ car-race/CarroVerde.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CarroVerde here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CarroVerde extends Actor
+{
+    /**
+     * Act - do whatever the CarroVerde wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX(), getY() + Greenfoot.getRandomNumber(10));
+     
+        if(isTouching(Car.class)) {
+            Greenfoot.stop();
+            getWorld().showText("GAME OVER", getWorld().getWidth()/2, getWorld().getHeight()/2);
+            MyWorld.gs.stop();
+            new GreenfootSound("hit.mp3").play();
+        }
+           if(isAtEdge()) {
+            getWorld().removeObject(this);
+            MyWorld.PONTOS++;
+        }
+    }
+}
