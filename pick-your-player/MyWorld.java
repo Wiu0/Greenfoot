5a73f9c5f6295b3f5d4b1c65facbfe810378f010
Wiu <williamnoronha00@gmail.com>
2025-08-16 11:43:26 -0300

    fix logic on mouse to pick a player

--- pick-your-player/MyWorld.java
+++ pick-your-player/MyWorld.java
@@ -19,7 +19,6 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         prepare();
     }
@@ -40,11 +39,10 @@ public class MyWorld extends World
     double time = 60;
     public void act() {
         showText("T: " + actorPicked, 200, 100);
+        mouseLogic();
         if(delay(0.1)) return;
 
         keyboardLogic();
-        mouseLogic();
-
     }
 
     private void mouseLogic() {
